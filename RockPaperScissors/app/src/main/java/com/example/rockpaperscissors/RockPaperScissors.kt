8a6a515707afacab5eb4783e5ff7ca504516a9d8
guehara-code <gil_uehara@yahoo.com.br>
2024-08-23 15:57:17 -0300

    rpc example - app logics included

---
+++ RockPaperScissors/app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -0,0 +1,48 @@
+package com.example.rockpaperscissors
+
+fun main() {
+    var computerChoice = ""
+    var playerChoice = ""
+
+    println("Rock, Paper or Scissors? Enter your choice!")
+    playerChoice = readln()
+
+    val randomNumber = (1..3).random()
+
+//    if(randomNumber == 1) {
+//        computerChoice = "Rock"
+//    } else if(randomNumber == 2) {
+//        computerChoice = "Paper"
+//    } else if(randomNumber == 3) {
+//        computerChoice = "Scissors"
+//    }
+
+    when(randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        3 -> {
+            computerChoice = "Scissors"
+        }
+    }
+
+    println(computerChoice)
+
+    val winner = when{
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
+
+    if(winner == "Tie") {
+        println("It's a tie")
+    } else {
+        println("$winner won!")
+
+    }
+}
