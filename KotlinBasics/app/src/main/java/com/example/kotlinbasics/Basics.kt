ea2305764b1983f83adac89dfb40db44f6326d40
guehara-code <gil_uehara@yahoo.com.br>
2024-08-24 07:57:05 -0300

    basics example - function call

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -2,11 +2,24 @@ package com.example.kotlinbasics
 
 fun main() {
 
-    var pi = 3.14f
+    // call function
+    makeCoffee()
 
-    println(pi)
 
-    pi = 3.1415926535f
-    println(pi)
+//    var pi = 3.14f
+//
+//    println(pi)
+//
+//    pi = 3.1415926535f
+//    println(pi)
+
+}
+
+fun makeCoffee() {
+    println("Buy beans")
+    println("Grind beans")
+    println("Heat water to 90C")
+    println("Add water")
+    println("Push hot water through the grinded beans")
 
 }
