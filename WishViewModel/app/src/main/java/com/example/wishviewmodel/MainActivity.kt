17bab0c838d3b6bbb3203040bd5fe95b844970e2
guehara-code <gil_uehara@yahoo.com.br>
2024-09-14 08:56:54 -0300

    shopping list app - ui initial implementation

--- WishViewModel/app/src/main/java/com/example/wishviewmodel/MainActivity.kt
+++ WishViewModel/app/src/main/java/com/example/wishviewmodel/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.wishviewmodel
 
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,14 +21,14 @@ import com.example.wishviewmodel.ui.theme.WishViewModelTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+      
         setContent {
             WishViewModelTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeView()
                 }
             }
         }
