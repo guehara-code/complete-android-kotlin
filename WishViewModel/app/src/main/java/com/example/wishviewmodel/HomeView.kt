e6efe5ffbe46b9444fd7fba4c0c9fab2f59c3f07
guehara-code <gil_uehara@yahoo.com.br>
2024-09-16 08:16:15 -0300

    shopping list app - floating action button included

--- WishViewModel/app/src/main/java/com/example/wishviewmodel/HomeView.kt
+++ WishViewModel/app/src/main/java/com/example/wishviewmodel/HomeView.kt
@@ -5,10 +5,17 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
@@ -18,7 +25,19 @@ fun HomeView() {
             AppBarView(title = "WishList", {
                 Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
             })
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                onClick = {
+                    Toast.makeText(context, "FAButton Clicked", Toast.LENGTH_LONG).show()
+                }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
         }
+
     ) {
         LazyColumn(
             modifier = Modifier
